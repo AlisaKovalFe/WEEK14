7e50d777373606f74df2eabce518717ad95ec48c
Alisa <alisakovalhome@mail.ru>
2022-11-10 19:50:35 +0300

    Add more in readme

--- task3.js
+++ task3.js
@@ -1,8 +1,6 @@
 function goNext() {
     let imageChange = document.querySelector('#imageNew');
     imageChange.src = './assets/pancake.png';
-    // let imageSecondChange = imageChange.querySelector('#imageNew');
-    // imageSecondChange.src = './assets/maffin.png';
 }
 
 function goPrevious() {
